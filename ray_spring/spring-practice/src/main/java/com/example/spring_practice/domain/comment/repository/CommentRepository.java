14f47f8d03cd448814a5cfed757da8a06b621cdf
epdlrnldudnj <kje0023@naver.com>
2025-11-03 19:47:25 +0900

    refactor : Repository 수정

--- ray_spring/spring-practice/src/main/java/com/example/spring_practice/domain/comment/repository/CommentRepository.java
+++ ray_spring/spring-practice/src/main/java/com/example/spring_practice/domain/comment/repository/CommentRepository.java
@@ -1,7 +1,9 @@
 package com.example.spring_practice.domain.comment.repository;
 
 import com.example.spring_practice.domain.comment.entity.Comment;
-import com.example.spring_practice.domain.shared.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface CommentRepository extends Repository<Comment> {
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
