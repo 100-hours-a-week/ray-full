380d324ebfe50e1fffeb57adab9098fdf821e501
epdlrnldudnj <kje0023@naver.com>
2025-11-03 19:46:42 +0900

    refactor : Entity 수정

--- ray_spring/spring-practice/src/main/java/com/example/spring_practice/domain/comment/entity/Comment.java
+++ ray_spring/spring-practice/src/main/java/com/example/spring_practice/domain/comment/entity/Comment.java
@@ -5,23 +5,40 @@ import com.example.spring_practice.domain.member.entity.Member;
 import com.example.spring_practice.domain.post.entity.Post;
 import com.example.spring_practice.global.response.CustomException;
 import com.example.spring_practice.global.response.ErrorCode;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
+
     private String content;
+
+    @CreationTimestamp
     private LocalDateTime createdAt;
+
     private boolean deleted = false;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
     private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id")
     private Post post;
 
     public Comment(CommentRequestDto commentRequestDto, Member member, Post post){
         this.content = commentRequestDto.getContent();
-        this.createdAt = LocalDateTime.now();
         this.member = member;
         this.post = post;
     }
--- ray_spring/spring-practice/src/main/java/com/example/spring_practice/domain/member/entity/Member.java
+++ ray_spring/spring-practice/src/main/java/com/example/spring_practice/domain/member/entity/Member.java
@@ -1,28 +1,47 @@
 package com.example.spring_practice.domain.member.entity;
 
+import com.example.spring_practice.domain.comment.entity.Comment;
+import com.example.spring_practice.domain.post.entity.Post;
+import com.example.spring_practice.domain.post.entity.PostLike;
 import com.example.spring_practice.global.response.CustomException;
 import com.example.spring_practice.global.response.ErrorCode;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
+
     private String email;
+
     private String password;
+
     private String nickname;
+
     private String profileImgUrl;
+
+    @OneToMany(mappedBy = "member")
+    private List<Post> postList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<Comment> commentList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<PostLike> postLikeList = new ArrayList<>();
 
     public Member(String email, String password, String nickname){
         this.email = email;
         this.password = password;
         this.nickname = nickname;
-    }
-
-    public void setMemberId(Long id){
-        if( this.memberId != null || id == null || id < 0){
-            throw new CustomException(ErrorCode.SERVER_ERROR);
-        }
-        this.memberId = id;
     }
 
     public void updateImageUrl(String profileImgUrl) { this.profileImgUrl = profileImgUrl; }
